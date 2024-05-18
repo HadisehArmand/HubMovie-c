b79a76e62aceaf828d80abcf597bc0337a17db7f
Hasdiseh <133518815+HadisehArmand@users.noreply.github.com>
2024-05-18 23:27:22 +0330

    update list-movie.component.ts

--- src/app/list-movie/list-movie.component.ts
+++ src/app/list-movie/list-movie.component.ts
@@ -1,10 +1,16 @@
-import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
+import { RouterOutlet, ActivatedRoute, Params } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { BaseService } from '../base-component/base.service';
-import { ActivatedRoute } from '@angular/router';
+import { Subscription, take } from 'rxjs';
 
 @Component({
   selector: 'list-movie',
@@ -18,26 +24,34 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './list-movie.component.html',
   styleUrls: ['./list-movie.component.scss'],
 })
-export class ListMovieComponent implements OnInit {
+export class ListMovieComponent implements OnInit, OnDestroy {
   @ViewChild('searchResults') searchResults: ElementRef<HTMLDivElement>;
   queryParamName: string = '';
   isGener: boolean = false;
+  private routeSub: Subscription | undefined;
 
   constructor(public baseService: BaseService, private route: ActivatedRoute) {
     this.searchResults = {} as ElementRef<HTMLDivElement>;
   }
 
   ngOnInit(): void {
-    this.route.data.subscribe((data) => {
+    this.routeSub = this.route.data.subscribe((data) => {
       this.queryParamName = data['queryParamName'];
       this.isGener = data['isGener'];
-      this.route.params.subscribe((params) => {
+
+      this.route.params.subscribe((params: Params) => {
         this.baseService.queryParam = params[this.queryParamName];
         this.baseService.pageRange = 1;
         this.baseService.isGener = this.isGener;
         this.baseService.fetchCategoryData();
       });
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
   }
 
   onMovieClick(movieId: number): void {
